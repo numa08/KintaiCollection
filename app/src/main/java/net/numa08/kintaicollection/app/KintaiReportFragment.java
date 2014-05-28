a1398dcc57b5f1e6383bd800424e3715625317ca
numa08 <n511287@gmail.com>
2014-05-28 23:16:21 +0900

    initiali commit

---
+++ app/src/main/java/net/numa08/kintaicollection/app/KintaiReportFragment.java
@@ -0,0 +1,16 @@
+package net.numa08.kintaicollection.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KintaiReportFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kintai_report, container, false);
+    }
+
+}
