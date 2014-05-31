8174cc79bb53f431044fe00772bba13a0c585393
numa08 <n511287@gmail.com>
2014-06-01 08:37:59 +0900

    set list adapter to list view

--- app/src/main/java/net/numa08/kintaicollection/app/KintaiListFragment.java
+++ app/src/main/java/net/numa08/kintaicollection/app/KintaiListFragment.java
@@ -16,16 +16,19 @@ import android.widget.TextView;
 
 import net.numa08.kintaicollection.app.domein.KintaiTimelineLoader;
 import net.numa08.kintaicollection.app.dummy.DummyContent;
+import net.numa08.kintaicollection.app.models.timeline.Kintai;
 import net.numa08.kintaicollection.app.models.timeline.KintaiTimelineItem;
 
 import java.util.List;
 
+import fj.Effect;
 import fj.F;
+import fj.P2;
 import fj.data.Option;
 
-public class KintaiListFragmentFragment extends ListFragment implements AbsListView.OnItemClickListener ,LoaderManager.LoaderCallbacks<List<KintaiTimelineItem>>{
+public class KintaiListFragment extends ListFragment implements AbsListView.OnItemClickListener ,LoaderManager.LoaderCallbacks<List<KintaiTimelineItem>>{
 
-    private Option<ListAdapter> adapter = Option.none();
+    private Option<ArrayAdapter<KintaiTimelineItem>> adapter = Option.none();
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -52,6 +55,12 @@ public class KintaiListFragmentFragment extends ListFragment implements AbsListV
 
     @Override
     public void onLoadFinished(Loader<List<KintaiTimelineItem>> loader, List<KintaiTimelineItem> data) {
+        adapter.bindProduct(Option.fromNull(data)).foreach(new Effect<P2<ArrayAdapter<KintaiTimelineItem>, List<KintaiTimelineItem>>>() {
+            @Override
+            public void e(P2<ArrayAdapter<KintaiTimelineItem>, List<KintaiTimelineItem>> product) {
+                product._1().addAll(product._2());
+            }
+        });
     }
 
     @Override
