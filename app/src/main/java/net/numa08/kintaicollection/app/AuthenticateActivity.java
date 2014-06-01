ffb650c06504c97aef3ea0556fef12279d8e7633
numa08 <n511287@gmail.com>
2014-06-01 12:35:07 +0900

    fix activity name

---
+++ app/src/main/java/net/numa08/kintaicollection/app/AuthenticateActivity.java
@@ -0,0 +1,19 @@
+package net.numa08.kintaicollection.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class AuthenticateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authoricate);
+    }
+    public void signIn(View view) {
+    }
+}
