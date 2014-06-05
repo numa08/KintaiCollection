32ae9e184ed895ee75a193340816502563ddf234
numa08 <n511287@gmail.com>
2014-06-05 23:45:15 +0900

    fix login callback method
    implements error handling

--- app/src/main/java/net/numa08/kintaicollection/app/AuthenticateActivity.java
+++ app/src/main/java/net/numa08/kintaicollection/app/AuthenticateActivity.java
@@ -1,8 +1,13 @@
 package net.numa08.kintaicollection.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceAuthenticationProvider;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
@@ -14,20 +19,40 @@ import net.numa08.kintaicollection.app.domein.api.KintaiCollectionWebClient;
 import net.numa08.kintaicollection.app.domein.api.request.FetchUserAccountRequest;
 import net.numa08.kintaicollection.app.models.timeline.User;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AUTH;
+
 import java.net.MalformedURLException;
 
 import fj.Effect;
+import fj.F;
+import fj.P2;
+import fj.Unit;
+import fj.data.Either;
 import fj.data.Option;
+import fj.test.reflect.Main;
 
 
 public class AuthenticateActivity extends Activity implements UserAuthenticationCallback {
+
+    public static final String KEY_USER_TOKEN = AuthenticateActivity.class.getName() + "KEY_USER_TOKEN";
+    public static final String KEY_USER_ID = AuthenticateActivity.class.getName() + "KEY_USER_ID";
+
 
     private Option<MobileServiceClient> azureClient = Option.none();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authoricate);
+
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (preferences.contains(AuthenticateActivity.KEY_USER_TOKEN) && preferences.contains(AuthenticateActivity.KEY_USER_ID)) {
+            final Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            setContentView(R.layout.activity_authoricate);
+        }
 
         try {
             azureClient = Option.fromNull(new MobileServiceClient(getText(R.string.azure_endpoint).toString(), getText(R.string.azure_key).toString(), this));
@@ -47,6 +72,37 @@ public class AuthenticateActivity extends Activity implements UserAuthentication
 
     @Override
     public void onCompleted(MobileServiceUser mobileServiceUser, Exception e, ServiceFilterResponse serviceFilterResponse) {
+        final Either<Exception, MobileServiceUser> either;
+        if (e == null) {
+            either = Either.right(mobileServiceUser);
+        } else {
+            either = Either.left(e);
+        }
+        either.right()
+               .map(new F<MobileServiceUser, Unit>() {
+                  @Override
+                  public Unit f(MobileServiceUser mobileServiceUser) {
+                      final String id = mobileServiceUser.getUserId();
+                      final String token = mobileServiceUser.getAuthenticationToken();
+                      PreferenceManager.getDefaultSharedPreferences(AuthenticateActivity.this)
+                              .edit()
+                              .putString(AuthenticateActivity.KEY_USER_ID, id)
+                              .putString(AuthenticateActivity.KEY_USER_TOKEN, token)
+                              .commit();
+                      final Intent intent = new Intent(AuthenticateActivity.this, MainActivity.class);
+                      startActivity(intent);
+                      finish();
+                      return Unit.unit();
+                  }
+               })
+               .left()
+               .foreach(new Effect<Exception>() {
+                  @Override
+                  public void e(Exception e) {
+                      Toast.makeText(AuthenticateActivity.this, R.string.msg_login_failed, Toast.LENGTH_LONG).show();
+                      Log.e(getString(R.string.app_name),"Login failed", e);
+                  }
+               });
     }
 
 }
